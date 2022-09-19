7522a51c72b27d13949e1cce043ea7a467e7ff90
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-19 10:30:55 -0300

    aula 199 fix: update moongose

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect('mongodb://localhost/db_money');
+module.exports = mongoose.connect('mongodb://localhost/db_money', 
+{ useNewUrlParser: true });
 
 mongoose.Error.messages.general.required = 
     "O atributo '{PATH}' é obrigatório.";
